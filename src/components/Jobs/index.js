d22787316a4936392d4284b33c6836c45f7492c9
dinesh <dineshbiddika17@gmail.com>
2025-04-16 05:47:40 +0000

    initial commit

---
+++ src/components/Jobs/index.js
@@ -0,0 +1,14 @@
+import Navbar from '../Navbar'
+import JobProfileSection from '../JobProfileSection'
+import './index.css'
+
+const Jobs = () => (
+  <>
+    <Navbar />
+    <div className="job-profile-container">
+      <JobProfileSection />
+    </div>
+  </>
+)
+
+export default Jobs
